db96005807c15cd7e44cd8c139c28e55bb8334b5
Felipe-builder <felipesvascon@gmail.com>
2022-07-14 23:31:33 -0300

    assync's

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blue('vamos começar!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
-console.log(texto(paragrafo));
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(texto))
+    .catch((erro) => trataErro(erro))
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
+//     if (erro) {
+//       trataErro(erro)
+//     }
+//     console.log(chalk.green(data))
+//   })
+// }
+
+pegaArquivo('./arquivos/texto1.md');
+
