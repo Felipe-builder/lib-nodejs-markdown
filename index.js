12c4d8bc8850f6a8fefa57d3381bc1b186956f14
Felipe-builder <felipesvascon@gmail.com>
2022-07-15 22:10:58 -0300

    Capturando links

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+const texto = 'A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s]*)\)/gm
+
+  const arrayResultados = []
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2]})
+  }
+  const linksExtraidos = regex.exec(texto);
+  console.log(arrayResultados)
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
@@ -40,5 +54,8 @@ async function pegaArquivo(caminhoDoArquivo) {
 //   })
 // }
 
-pegaArquivo('./arquivos/texto1.md');
+// pegaArquivo('./arquivos/texto1.md');
+extraiLinks(texto)
 
+
+//      \[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s]*)\)
